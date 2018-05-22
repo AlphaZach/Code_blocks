4d68f3044e2f8d06f75cbc8bbfa54486f83da6cb
zach <zacharyczf@gmail.com>
2018-05-21 23:13:54 -0700

    JS 1st commit

---
+++ JS/JS_code_blocks.js
@@ -0,0 +1,54 @@
+/*forEach*/
+var strings = ["my", "forEach", "example"];
+var result = " ";
+//s -> str, i -> index, a -> array
+strings.forEach(function(s, i, a) {
+    if (a.length - 1 !== i) {
+       result += s + " ";
+    } else {
+      result += s + "!!!";
+    }
+});
+// >console.log(result)
+// my forEach example!!!
+/*-------------------------------------------------*/
+
+/*setTimeout*/
+var Id = setTimeout(function() { 
+	console.log("this function runs in 30 seconds"); 
+}, 3000);
+
+setTimeout(function() { 
+	console.log("cancel", Id); 
+	clearTimeout(Id);
+}, 2000);
+// < 68594         //The id for setTimeout
+// > cancel 68593
+/*-------------------------------------------------------*/
+
+/*setInterval*/
+var intervalId = setInterval(function() {
+num ++;
+console.log("num:", num);
+if (num === 3) {
+clearInterval(intervalId);
+}
+}, 1000);
+//> num: 1
+//> num: 2
+//> num: 3
+/*-----------------------------------------------------*/
+
+/*Queue and Stack*/
+setTimeout(function() {
+	console.log("Hello from the timeout");
+},0);
+for(var i = 0; i < 1000000000; i++) {
+	var x = i^2;
+}
+console.log("done with loop");
+//> done with the loop
+//> Hello from the timeout
+// the function in setTimeout is in a queue, and can't be invoked until the stack is empty
+/*---------------------------------------------------------*/
+
