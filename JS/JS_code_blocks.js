c39e5f97f0279667dc33b4122e5599bd4a12fd4e
zach <zacharyczf@gmail.com>
2018-05-22 17:30:45 -0700

    Promise, Promise Chain, XMLHttpRequest

--- JS/JS_code_blocks.js
+++ JS/JS_code_blocks.js
@@ -52,3 +52,64 @@ console.log("done with loop");
 // the function in setTimeout is in a queue, and can't be invoked until the stack is empty
 /*---------------------------------------------------------*/
 
+/*Promise*/
+ var p1 = new Promise(function(resolve, reject) {
+   var num = Math.random();
+   if (num < 0.5) {
+     resolve(num);
+   } else {
+     reject(num);
+   }
+ });
+
+ p1.then(function(result) {
+   console.log("Success:", result);
+ }).catch(function(error) {
+   console.log("Error:", error);
+ });
+ // note 'then' is fired when resolve, catch is for reject
+ /*------------------------------------------------------------*/
+
+/*Promise Chain*/
+//example a counter count each second, up tp 3
+ var counter = 0;
+ function incCounter() {
+   counter++;
+   console.log("Counter:", counter);
+ }
+
+ function runLater(callback, timeInMs) {
+   var p = new Promise(function(resolve, reject) {
+     setTimeout(function() {
+       var res = callback();
+       resolve(res);
+     }, timeInMs);
+   });
+   return p;
+ }
+
+ runLater(incCounter, 1000).then(function() {
+   return runLater(incCounter, 1000);
+ }).then(function() {
+   return runLater(incCounter, 1000);
+ });
+/*---------------------------------------------------*/
+
+/*XMLHttpRequest*/
+var XHR = new XMLHttpRequest();
+
+XHR.onreadystatechange = function() {
+	//readyState == 4 means the operation is complete
+  if(XHR.readyState == 4) {
+  	//status == 200 means a succueesful request
+    if(XHR.status == 200) {
+      console.log(XHR.responseText);
+    } else {
+      console.log("There was a problem!");
+    }
+  }
+}
+
+XHR.open("GET", "https://api.github.com/zen");
+XHR.send();
+/*------------------------------------------------------*/
